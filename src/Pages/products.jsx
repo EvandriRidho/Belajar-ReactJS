78d635da4c2ed63def71997334e384b2389f6d51
unknown <evandriridho555@gmail.com>
2024-07-22 18:54:33 +0700

    Update React

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,28 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    images: "/images/shoes-1.jpg",
+    title: "New Shoes",
+    price: "$ 120",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nisi distinctio incidunt facilis sunt a aliquam? Qui accusantium officiis`,
+  },
+];
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header images="/images/shoes-1.jpg" />
-        <CardProduct.Body title="New Shoes">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nisi
-          distinctio incidunt facilis sunt a aliquam? Qui accusantium officiis
-          aspernatur? Iure eligendi eius laborum ea alias, accusamus voluptate
-          nulla! Minima!
-        </CardProduct.Body>
-        <CardProduct.Footer price="$ 120" />
-      </CardProduct>
+      {products.map((product) => {
+        return (
+          <CardProduct>
+            <CardProduct.Header images={product.images} />
+            <CardProduct.Body title={product.title}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        );
+      })}
     </div>
   );
 };
